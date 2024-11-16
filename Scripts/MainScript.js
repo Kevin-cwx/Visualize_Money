d295a63cb823ad82262024c346b4736d3f0415f9
kevin Foendoe <kevin@businessone.cw>
2024-11-16 10:09:35 -0400

    Moved js to folder

---
+++ Scripts/MainScript.js
@@ -0,0 +1,120 @@
+var log = console.log;
+var MainValue = 10;
+SetMainValue(MainValue);
+var InputFieldMainValueID = document.getElementById('InputFieldMainValueID');
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+$("#MainValue").text('FL ' + MainValue);
+
+function SetMainValue(InputData) {
+    log(InputData);
+    $("#MainValue").text('FL ' + numberWithCommas(InputData));
+    MainValue = InputData;
+    PerformCalculations();
+}
+
+var BillsOf100, BillsOf50, BillsOf25, BillsOf10, BillsOf5, BillsOf1 = 0
+
+function PerformCalculations() {
+
+    BillsOf100 = Math.floor(MainValue / 100);
+    BillsOf50 = Math.floor((MainValue - (BillsOf100 * 100)) / 50);
+    BillsOf25 = Math.floor(((MainValue - (BillsOf100 * 100)) - (BillsOf50 * 50)) / 25);
+    BillsOf10 = Math.floor(((MainValue - (BillsOf100 * 100)) - (BillsOf50 * 50) - (BillsOf25 * 25)) / 10);
+    BillsOf5 = Math.floor(((MainValue - (BillsOf100 * 100)) - (BillsOf50 * 50) - (BillsOf25 * 25) - (BillsOf10 * 10)) / 5);
+    BillsOf1 = Math.floor(((MainValue - (BillsOf100 * 100)) - (BillsOf50 * 50) - (BillsOf25 * 25) - (BillsOf10 * 10) - (BillsOf5 * 5)) / 1);
+
+    log("Main Value: " + MainValue + "\n\n\n\n")
+    log("Bills of 100: " + BillsOf100)
+    log("Bills of 50: " + BillsOf50)
+    log("Bills of 25: " + BillsOf25)
+    log("Bills of 10: " + BillsOf10)
+    log("Bills of 5: " + BillsOf5)
+    log("Bills of 1: " + BillsOf1)
+
+    log("\n\nSum of Bills: " + Number(BillsOf100 * 100 + BillsOf50 * 50 + BillsOf25 * 25 + BillsOf10 * 10 + BillsOf5 * 5 + BillsOf1 * 1));
+    //PrintAllBills();
+
+
+    const sleep = (s) =>
+        new Promise((p) => setTimeout(p, (s * 250) | 0))
+
+    async function timeoutHandler() {
+        await sleep(1)
+        PrintAllBills();
+    }
+
+    setTimeout(timeoutHandler, 150)
+
+}
+
+/* Toggle */
+const toggle = document.getElementById('toggle');
+const statusText = document.getElementById('status');
+
+// Function to update the status text based on toggle state
+function updateStatus() {
+    if (toggle.checked) {
+       log('New_Bills');
+       Folder_Name = "New_Bills"
+    } else {
+        log('Old_Bills');
+        Folder_Name = "Bills"
+    }
+    PerformCalculations();
+}
+
+// Event listener to detect changes in toggle state
+toggle.addEventListener('change', updateStatus);
+
+// Initial status update
+updateStatus();
+/* */
+//Folder_Name = "New_Bills"
+function PrintN_Times(AmountOfTimes, BillType) {
+    for (let index = 0; index < AmountOfTimes; index++) {
+
+        $(".ImageWrapper").append('<img class="BillsOfImage" src=../Media/'+Folder_Name+'/FL' + BillType + '.png>')
+    }
+}
+
+function PrintAllBills() {
+    $(".ImageWrapper").empty();
+
+    PrintN_Times(BillsOf1, 1)
+    PrintN_Times(BillsOf5, 5)
+    PrintN_Times(BillsOf10, 10)
+    PrintN_Times(BillsOf25, 25)
+    PrintN_Times(BillsOf50, 50)
+    PrintN_Times(BillsOf100, 100)
+}
+
+InputFieldMainValueID.focus();
+
+//Keeps focus in input field
+InputFieldMainValueID.onblur = function (event) {
+    var blurEl = this;
+    setTimeout(function () {
+        blurEl.focus()
+    }, 10);
+};
+
+//allows for only numeric input
+InputFieldMainValueID.onkeypress = function (event) {
+    return event.charCode >= 48 && event.charCode <= 57;
+};
+
+//Changes value on input
+InputFieldMainValueID.oninput = function () {
+    SetMainValue(InputFieldMainValueID.value);
+};
+
+//Disables multiple zeros. Ex 00000
+InputFieldMainValueID.addEventListener("input", function () {
+    if (this.value.length === 1 && this.value === "0") {
+        this.value = "";
+    }
+});
